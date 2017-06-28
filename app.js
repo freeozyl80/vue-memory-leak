fbf6e985ad69da41e0480761a3810738a7503ad2
jaffezhang <jaffezhang@tencent.com>
2017-06-28 10:42:25 +0800

    appen

--- app.js
+++ app.js
@@ -1,25 +1,27 @@
 'use strict';
 
 const path = require('path');
-const express = require('express');
 const Vue = require('vue/dist/vue.runtime.common.js');
 const VueRender = require('vue-server-renderer').createRenderer();
 
-const App = {
-	template: '<div>{{test}}</div>',
-	computed: {
-		"test": function () {
-			throw 'erro happened';
-			return 1;
+
+var timer = setInterval(function(argument) {
+	const App = {
+		template: '<div>{{test}}</div>',
+		computed: {
+			"test": function() {
+				throw 'erro happened';
+				return 1;
+			}
 		}
 	}
-}
-const vm = new Vue(App);
+	const vm = new Vue(App);
 
-VueRender.renderToString(vm, function (error, html) {
-	if (error) {
-		console.log('error happened', error)
-	} else {
-		console.log(html);
-	}
-})
+	VueRender.renderToString(vm, function(error, html) {
+		if (error) {
+			console.log('error happened', error)
+		} else {
+			console.log(html);
+		}
+	})
+}, 10);
